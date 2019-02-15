426f494b0c6cbae1365096f90e75f94950acc34c
Maciej Hirsz <1096222+maciejhirsz@users.noreply.github.com>
2019-02-15 10:28:16 +0100

    Cargo feature to switch to pure Rust libsecp256k1 (#7)
    * feat: Cargo feature to switch to pure Rust libsecp256k1
    
    * fix: Expose the ec::Error type for API consumers
    
    * feat: Add SecretKey::from_crypto
    
    * feat: Add SecretKey::as_bytes
    
    * feat: Replace SecretKey::from_crypto with Crypto::decrypt
    
    * feat: Add Crypto::encrypt
    
    * feat: Cleanup the Error story; remove SecretKey::as_bytes
    
    * chore: Bump version

---
+++ src/error.rs
@@ -0,0 +1,32 @@
+//! The error type
+
+use crate::ec;
+
+/// Key error
+#[derive(Debug)]
+pub enum Error {
+    /// Invalid password for the keyfile
+    InvalidPassword,
+    /// Crypto error
+    Crypto(parity_crypto::Error),
+    /// Secp256k1 error
+    Secp256k1(crate::ec::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::InvalidPassword => write!(fmt, "Invalid Password"),
+            Error::Crypto(ref e) => write!(fmt, "Crypto: {}", e),
+            Error::Secp256k1(ref e) => write!(fmt, "Secp256k1: {:?}", e),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<ec::Error> for Error {
+	fn from(e: ec::Error) -> Error {
+		Error::Secp256k1(e)
+	}
+}
