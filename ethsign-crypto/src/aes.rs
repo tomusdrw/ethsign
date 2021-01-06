2251a0de4cf39b11e9398e904eec3a7ecb8b2bdf
Tomasz Drwięga <tomusdrw@gmail.com>
2021-01-06 11:35:35 +0100

    Update other crypto stuff.

--- ethsign-crypto/src/aes.rs
+++ ethsign-crypto/src/aes.rs
@@ -2,8 +2,8 @@
 
 use std::fmt;
 use aes_ctr::Aes128Ctr;
-use aes_ctr::stream_cipher::generic_array::GenericArray;
-use aes_ctr::stream_cipher::{NewStreamCipher, SyncStreamCipher};
+use aes_ctr::cipher::generic_array::GenericArray;
+use aes_ctr::cipher::{NewStreamCipher, SyncStreamCipher};
 
 /// Error type for the AES symmetric encryption
 #[derive(Debug)]
